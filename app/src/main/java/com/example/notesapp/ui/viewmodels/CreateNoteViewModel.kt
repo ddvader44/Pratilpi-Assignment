4948866b81961199dc34e4891bb5e997ea8e1ecc
Dishit <40226856+ddvader44@users.noreply.github.com>
2022-01-17 19:30:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/ui/viewmodels/CreateNoteViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.notesapp.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.notesapp.database.NotesDatabase
+import kotlinx.coroutines.launch
+
+class CreateNoteViewModel : ViewModel() {
+//    var selectedColor = "#171C26"
+//    var currentDate:String? = null
+//    private var READ_STORAGE_PERM = 123
+//    private var REQUEST_CODE_IMAGE = 456
+//    private var selectedImagePath = ""
+//    private var webLink = ""
+//    private var noteId = -1
+
+
+
+}
